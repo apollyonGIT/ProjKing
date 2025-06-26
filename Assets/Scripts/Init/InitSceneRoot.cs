9e48df0a5e18ed3489af49814c00423daa04c8c6
zhangjunchi <zhangjunchi@conchship.net>
2025-06-26 16:18:17 +0800

    --Add 1617 1. 基础代码。

---
+++ Assets/Scripts/Init/InitSceneRoot.cs
@@ -0,0 +1,45 @@
+﻿using Commons;
+using Foundations;
+using Foundations.SceneLoads;
+
+namespace Init
+{
+    public class InitSceneRoot : SceneRoot<InitSceneRoot>
+    {
+        public string next_scene_name;
+
+        //==================================================================================================
+
+        protected override void on_init()
+        {
+            base.on_init();
+        }
+
+
+        protected override void on_fini()
+        {
+            base.on_fini();
+        }
+
+
+        public void btn_start_new_game()
+        {
+            Game_Mgr.on_start_new_game();
+
+            SceneLoad_Utility.load_scene_with_loading(next_scene_name);
+        }
+
+
+        public void btn_exit()
+        {
+            Game_Mgr.on_exit_game();
+
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
+
